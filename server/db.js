81b74aedf3ed8e650f55adcba9f99cdd69ead3de
Owen <owensch@iastate.edu>
2021-08-28 19:52:34 -0700

    Fix3

--- server/db.js
+++ server/db.js
@@ -3,6 +3,9 @@ const dev = require('./devConfig.json')
 const connectionString = process.env.DATABASE_URL || dev['db-url']
 const pool = new Pool({
   connectionString,
+    ssl: {
+    rejectUnauthorized: false
+  },
 })
 
 module.exports = pool;
